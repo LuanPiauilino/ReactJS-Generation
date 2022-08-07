a667030a37dfeb02a2f9d978160b33ad8a9b9846
LuanPiauilino <luanpsilva2001@gmail.com>
2022-08-07 19:38:41 -0300

    GetAll da model Tema e Postagem

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,11 @@ import Footer from './components/estaticos/footer/Footer';
 import Home from './paginas/home/Home';
 import Login from "./paginas/login/Login";
 import CadastroUsuario from "./paginas/cadastrousuario/CadastroUsuario";
+import ListaTema from './components/temas/listatema/ListaTema';
+import ListaPostagem from './components/postagens/listapostagem/ListaPostagem';
 
 import './App.css';
+
 
 
 function App() {
@@ -21,6 +24,8 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/home" element={<Home />} />
           <Route path="/cadastro" element={<CadastroUsuario/>} />
+          <Route path="/temas" element={<ListaTema />} />
+        <Route path="/posts" element={<ListaPostagem />} />
         </Routes>
       </div>
       <Footer />
